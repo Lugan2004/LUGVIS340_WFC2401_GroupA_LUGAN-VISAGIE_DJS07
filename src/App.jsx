3fd7d982186823f18bb261bc3a852d03ae1d4956
Lugan2004 <visagielugan42@gmail.com>
2024-05-30 14:48:25 +0200

    added  header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-
+// eslint-disable-next-line no-unused-vars
+import React from "react"
+import Header from "../components/header"
 function App() {
 
   return (
-    <>
-     
-    </>
+    <div>
+      <Header/>
+    </div>
   )
 }
 
