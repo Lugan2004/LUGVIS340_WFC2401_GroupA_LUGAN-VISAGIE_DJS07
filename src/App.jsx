6c72166a17005a87440eb00816125c212426cfc8
Lugan2004 <visagielugan42@gmail.com>
2024-05-31 13:49:40 +0200

    added meme

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react"
 import Header from "../components/header"
+import Meme from "../components/Meme"
 function App() {
 
   return (
     <div>
       <Header/>
+      <Meme/>
     </div>
   )
 }
